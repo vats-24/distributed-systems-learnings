296c3583687bb14b93df9c55696175c3db14b8ec
vats-24 <vasu.70.mishra@gmail.com>
2026-05-15 11:02:51 +0530

    phase-racecondn

--- single-node/main.go
+++ single-node/main.go
@@ -1,28 +1,122 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 )
 
 var startedAt time.Time
 
+var totalRequests int
+
+var mutex sync.Mutex
+
+type HealthRespone struct {
+	Status string "json:status"
+	Uptime string "json:uptime"
+}
+
+type StatsResponse struct {
+	TotalRequests int "json:total_requests"
+}
+
+func incrementRequestCount() {
+	mutex.Lock()
+	//critical section requires locking for concurrent requests behaviour
+	totalRequests++
+
+	mutex.Unlock()
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintln(w, "Hello from GO HTTP Server")
+	incrementRequestCount()
+
+	hostname, err := os.Hostname()
+
+	if err != nil {
+		hostname = "unknown"
+	}
+
+	fmt.Fprintf(w, "Hello from GO HTTP Server running on %s\n", hostname)
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+
+	incrementRequestCount()
+
+	uptime := time.Since(startedAt)
+
+	response := HealthRespone{
+		Status: "ok",
+		Uptime: uptime.String(),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewEncoder(w).Encode(response)
+
+	if err != nil {
+		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
+		return
+	}
+}
+
+func statsHandler(w http.ResponseWriter, r *http.Request) {
+
+	incrementRequestCount()
+	//locking before sharing same reading state
+	mutex.Lock()
+
+	response := StatsResponse{
+		TotalRequests: totalRequests,
+	}
+
+	mutex.Unlock()
+
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewEncoder(w).Encode(response)
+
+	if err != nil {
+		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
+		return
+	}
+
 }
 
 func main() {
 
+	startedAt = time.Now()
+
+	//they are automatically concurrent go handles them
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/health", healthHandler)
+	http.HandleFunc("/stats", statsHandler)
 
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		port = "8080"
 	}
+
+	server := &http.Server{
+		Addr: ":" + port,
+	}
+
+	go func() {
+		fmt.Println("Server starting on port", port)
+
+		err := server.ListenAndServe()
+
+		if err != nil && err != http.ErrServerClosed {
+			fmt.Println("Server Failed:", err)
+		}
+	}()
 
 	fmt.Println("Server starting at port", port)
 
