9b5e263be1cad710250186dd8758e17d36178e72
vats-24 <vasu.70.mishra@gmail.com>
2026-05-22 00:28:20 +0530

    phase/metrics-adapter:-metrics_registration

--- metrics-adapter/internal/metrics/metrics.go
+++ metrics-adapter/internal/metrics/metrics.go
@@ -1,1 +1,25 @@
 package metrics
+
+import "github.com/prometheus/client_golang/prometheus"
+
+var AdapterScrapesTotal = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "adapter_scrapes_total",
+		Help: "Total unsuccessful adapter scrapes",
+	},
+	[]string{"service"},
+)
+
+var FoundationRequests = prometheus.NewGaugeVec(
+	prometheus.GaugeOpts{
+		Name: "foundation_requests",
+		Help: "Current request count from foundation service",
+	},
+	[]string{"service"},
+)
+
+func Register() {
+	prometheus.MustRegister(AdapterScrapesTotal)
+
+	prometheus.MustRegister(FoundationRequests)
+}
