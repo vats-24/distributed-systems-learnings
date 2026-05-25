20d641022dc237e707433a5f7463ef1ba25898bd
vats-24 <vasu.70.mishra@gmail.com>
2026-05-25 13:06:19 +0530

    phase/metrics-adapter:-graceful_shutdown

--- metrics-adapter/main.go
+++ metrics-adapter/main.go
@@ -1,22 +1,49 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"metrics-adapter/internal/metrics"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"metrics-adapter/internal/metrics"
+	"metrics-adapter/internal/scraper"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
+	ctx, cancel := context.WithCancel(
+		context.Background(),
+	)
+
+	defer cancel()
 
 	fmt.Println("Metrics adapter listening on :9090")
 
 	metrics.Register()
 
-	// go scraper.Start()
+	go scraper.Start(ctx)
 
 	http.Handle("/metrics", promhttp.Handler())
+
+	signalChan := make(chan os.Signal, 1)
+
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+
+		sig := <-signalChan
+
+		fmt.Println(
+			"Received shutdown signal:",
+			sig,
+		)
+
+		cancel()
+	}()
 
 	err := http.ListenAndServe(":9090", nil)
 
