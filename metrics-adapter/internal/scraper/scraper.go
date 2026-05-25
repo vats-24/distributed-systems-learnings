ff84d8b64662c402ef9bc28df71ee8e189e52839
vats-24 <vasu.70.mishra@gmail.com>
2026-05-25 12:56:19 +0530

    phase/metrics-adapter:-context_propagation

--- metrics-adapter/internal/scraper/scraper.go
+++ metrics-adapter/internal/scraper/scraper.go
@@ -1,6 +1,7 @@
 package scraper
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"metrics-adapter/internal/metrics"
@@ -12,42 +13,90 @@ type StatsResponse struct {
 	Requests int `json:"requests"`
 }
 
-func Start() {
+var client = &http.Client{
+
+	Transport: &http.Transport{
+		MaxIdleConns: 10,
+
+		MaxIdleConnsPerHost: 5,
+
+		IdleConnTimeout: 30 * time.Second,
+	},
+}
+
+func Start(ctx context.Context) {
 	ticker := time.NewTicker(10 * time.Second)
 
 	for {
-		<-ticker.C
 
-		fmt.Println("Scrapping foundation service...")
+		select {
 
-		resp, err := http.Get(
-			"http://localhost:8080/stats",
-		)
+		case <-ctx.Done():
 
-		if err != nil {
+			fmt.Println("Scrapper shutting down")
 
-			fmt.Println("Scrape failed:", err)
+			return
 
-			continue
+		case <-ticker.C:
+
+			fmt.Println("Scrapping foundation service...")
+
+			ctx, cancel := context.WithTimeout(
+				context.Background(),
+				3*time.Second,
+			)
+
+			defer cancel()
+
+			req, err := http.NewRequestWithContext(
+				ctx,
+				http.MethodGet,
+				"http://localhost:8080/stats",
+				nil,
+			)
+
+			if err != nil {
+
+				fmt.Println("Request creation failed:", err)
+
+				continue
+			}
+
+			if err != nil {
+
+				fmt.Println("Scrape failed:", err)
+
+				continue
+			}
+
+			resp, err := client.Do(req)
+
+			if err != nil {
+
+				fmt.Println("Scrape failed:", err)
+
+				continue
+			}
+
+			var stats StatsResponse
+
+			err = json.NewDecoder(resp.Body).Decode(&stats)
+
+			resp.Body.Close()
+
+			if err != nil {
+
+				fmt.Println("JSON decode failed:", err)
+
+				continue
+			}
+
+			metrics.FoundationRequests.WithLabelValues("foundation").Set(float64(stats.Requests))
+
+			metrics.AdapterScrapesTotal.WithLabelValues("foundation").Inc()
+
+			fmt.Println("Update metrics:", stats.Requests)
+
 		}
-
-		var stats StatsResponse
-
-		err = json.NewDecoder(resp.Body).Decode(&stats)
-
-		resp.Body.Close()
-
-		if err != nil {
-
-			fmt.Println("JSON decode failed:", err)
-
-			continue
-		}
-
-		metrics.FoundationRequests.WithLabelValues("foundation").Set(float64(stats.Requests))
-
-		metrics.AdapterScrapesTotal.WithLabelValues("foundation").Inc()
-
-		fmt.Println("Update metrics:", stats.Requests)
 	}
 }
